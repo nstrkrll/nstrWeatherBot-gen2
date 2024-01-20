8bf5c8d97a8e05704109382d9cfe7709b35cf0e9
nstrkrll <nesterkalol@gmail.com>
2024-01-20 20:52:36 +0300

    added bot commands logic

---
+++ nstrWeatherBot-gen2/Commands/HelpCommand.cs
@@ -0,0 +1,34 @@
+﻿using nstrWeatherBot_gen2.Services;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using nstrWeatherBot_gen2.Interfaces;
+using Telegram.Bot.Types.Enums;
+
+namespace nstrWeatherBot_gen2.Commands
+{
+    public class HelpCommand : ICommand
+    {
+        public TelegramBotClient Client => TelegramBot.GetTelegramBot();
+
+        public string Name => "/help";
+
+        public async Task Execute(Update update)
+        {
+            long chatId = update.Message.Chat.Id;
+            await Client.SendTextMessageAsync
+            (
+                chatId: chatId, 
+                text: $"Я \\- погодный бот\\!\n" +
+                    $"\nНапиши название населенного пункта\\, прогноз погоды которого хочешь узнать\\." +
+                    $"\nДля уточнения можешь указать страну\\, в которой находится нужный населенный пункт\\." +
+                    $"\n*Подойдет такой формат\\:*\n" +
+                    $"*_Витебск_*\n" +
+                    $"или\n" +
+                    $"*_Витебск, Беларусь_*\n" +
+                    $"или\n" +
+                    $"*_Витебск Россия Новосибирск_*", 
+                parseMode: ParseMode.MarkdownV2
+            );
+        }
+    }
+}
---
+++ nstrWeatherBot-gen2/Commands/WeatherCommand.cs
@@ -0,0 +1,24 @@
+﻿using nstrWeatherBot_gen2.Services;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using nstrWeatherBot_gen2.Interfaces;
+
+namespace nstrWeatherBot_gen2.Commands
+{
+    public class WeatherCommand : ICommand
+    {
+        public TelegramBotClient Client => TelegramBot.GetTelegramBot();
+
+        public string Name => "";
+
+        public async Task Execute(Update update)
+        {
+            long chatId = update.Message.Chat.Id;
+            await Client.SendTextMessageAsync
+            (
+                chatId: chatId,
+                text: WeatherService.GetWeatherClient().GetWeatherInfo(update.Message.Text)
+            );
+        }
+    }
+}
