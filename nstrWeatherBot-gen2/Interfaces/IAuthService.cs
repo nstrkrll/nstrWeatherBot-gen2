5f5583b99dede81e39d63f798414ed84e6b911f9
nstrkrll <nesterkalol@gmail.com>
2024-01-23 21:14:41 +0300

    added auth service, for auth on admin panel

---
+++ nstrWeatherBot-gen2/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using nstrWeatherBot_gen2.Models;
+
+namespace nstrWeatherBot_gen2.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<string> Login(UserLoginDTO userData);
+    }
+}
---
+++ nstrWeatherBot-gen2/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace nstrWeatherBot_gen2.Services
+{
+    public class AuthService
+    {
+        public static bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
