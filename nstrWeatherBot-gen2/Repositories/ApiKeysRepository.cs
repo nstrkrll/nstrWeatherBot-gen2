3b20a8e5a321030c99ce5d781620462ff826eebe
nstrkrll <nesterkalol@gmail.com>
2024-01-23 21:15:15 +0300

    added repository (pattern "repository") for database table data

---
+++ nstrWeatherBot-gen2/Repositories/ApiKeysRepository.cs
@@ -0,0 +1,47 @@
+﻿using nstrWeatherBot_gen2.Models;
+using nstrWeatherBot_gen2.Services;
+
+namespace nstrWeatherBot_gen2.Repositories
+{
+    public class ApiKeysRepository
+    {
+        private readonly nstrWeatherDBContext _context;
+        private bool _disposed = false;
+
+        public ApiKeysRepository(nstrWeatherDBContext context)
+        {
+            _context = context;
+        }
+
+        public ApiKeys Get(int id)
+        {
+            return _context.ApiKeys.FirstOrDefault(x => x.ApiKeyId == id);
+        }
+
+        public void Update(ApiKeys apiInstance)
+        {
+            _context.ApiKeys.Update(apiInstance);
+            _context.SaveChanges();
+            AccuWeather.GetWeatherClient().ApiKey = apiInstance.KeyString;
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
