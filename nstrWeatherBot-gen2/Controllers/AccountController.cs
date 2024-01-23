9fbaed90a7380830e3adb82bb101d0db79d58d56
nstrkrll <nesterkalol@gmail.com>
2024-01-23 21:16:24 +0300

    added auth controller

---
+++ nstrWeatherBot-gen2/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using nstrWeatherBot_gen2.Models;
+using nstrWeatherBot_gen2.Services;
+using System.Security.Claims;
+
+namespace nstrWeatherBot_gen2.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly nstrWeatherDBContext _context;
+
+        public AccountController(nstrWeatherDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _context.Users.FirstOrDefaultAsync(x => x.Email == model.Email);
+                if (user != null && AuthService.VerifyPassword(model.Password, user.PasswordHash, user.PasswordSalt))
+                {
+                    await Authenticate(model.Email);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            }
+
+            return View(model);
+        }
+
+        private async Task Authenticate(string email)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, email)
+            };
+
+            var id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
