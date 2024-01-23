ee0aa21f492b0ba69408fdf9e4fb77e5b4b22048
nstrkrll <nesterkalol@gmail.com>
2024-01-23 21:13:27 +0300

    Improvements of pattern Singleton for class TelegramBot (modified for threading)

--- nstrWeatherBot-gen2/Services/TelegramBot.cs
+++ nstrWeatherBot-gen2/Services/TelegramBot.cs
@@ -4,17 +4,20 @@ namespace nstrWeatherBot_gen2.Services
 {
     public class TelegramBot
     {
-        private static TelegramBotClient _client { get; set; }
+        private static TelegramBotClient _client;
+        private static object _lock = new object();
 
         public static TelegramBotClient GetTelegramBot()
         {
-            if (_client != null)
+            lock (_lock)
             {
+                if (_client == null)
+                {
+                    _client = new TelegramBotClient("5027490030:AAFo2kbzOnYcYo9N83KiQUhl5E7wTAv5asg");
+                }
+
                 return _client;
             }
-
-            _client = new TelegramBotClient("5027490030:AAGl_P-FAG3_v-SiaexZP9b7cjSbVlgE2OA");
-            return _client;
         }
     }
 }
