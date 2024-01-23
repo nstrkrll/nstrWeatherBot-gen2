3bd1418ebf30ee8d02cce4cad3909dff49d119b2
nstrkrll <nesterkalol@gmail.com>
2024-01-23 21:13:57 +0300

    added database context

---
+++ nstrWeatherBot-gen2/nstrWeatherDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace nstrWeatherBot_gen2
+{
+    public class nstrWeatherDBContext : DbContext
+    {
+        public DbSet<Models.User> Users { get; set; }
+        public DbSet<Models.ApiKeys> ApiKeys { get; set; }
+
+        public nstrWeatherDBContext(DbContextOptions<nstrWeatherDBContext> options) : base(options) { }
+    }
+}
